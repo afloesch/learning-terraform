791629c2c74050f49d67c360da8a4ef17b893fca
Andrew Loesch <afloesch@gmail.com>
2017-10-12 17:53:31 -0700

    update single machine in vpc example

--- single-machine-with-VPC/example.tf
+++ single-machine-with-VPC/example.tf
@@ -7,13 +7,13 @@ provider "aws" {
 # import the cert into amazon for accessing the boxes
 resource "aws_key_pair" "dev" {
   key_name   = "aws.test"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCqTct2kzoI8GR008xizhsPfg+lbnZLWlxxSBP5nu7gm0KT3W3e+wdNzoQU21f6B/PW1YAVShJZP7I/OIXLQ82bW0PnFWxzXi+f9bz+ETmiIgKzaPOEP6W2IHyygHHc6Wy5OD6aLP5yjRJcvoKJXLp2C1wvviJjsvY8+c9g7Nk1F40/MSR4QqBE+mX4QgF1saTBKwLgOrLlqgYgrDnlmi+x4837f4W1BfPT/ruFGkqhRXG2IgonSEbNtL3XZ1tuzod7CCyjyKQ83vy8sXn/U/j3t3kId4lh5JlCPO+Q67aAK1c0oAvDos6JVtmofWT14Ud+2oOcD5x4Xi9i0Z5SzwXh andrew.loesch@C02Q9FEKG8WN-L"
+  public_key = "${file(var.ssh_key)}"
 }
 
 # create a security group to expose web traffic ports to public
 resource "aws_security_group" "web" {
   name        = "Web Traffic"
-  description = "Allow all inbound traffic from http (80) and https (443)"
+  description = "Allow all inbound traffic from http (80)"
   vpc_id = "${aws_vpc.default.id}"
 
   tags {
@@ -40,12 +40,13 @@ resource "aws_security_group" "web" {
 resource "aws_instance" "node" {
     ami = "${var.aws_ami}"
     availability_zone = "us-west-1a"
-    instance_type = "t2.micro"
-    key_name = "${var.aws_key_name}"
-    vpc_security_group_ids = ["${aws_security_group.web.id}"]
-    subnet_id = "${aws_subnet.us-west-1a-public.id}"
+    instance_type = "m1.small"
+    key_name = "${aws_key_pair.dev.key_name}"
+    security_groups = ["${aws_security_group.web.name}"]
+    associate_public_ip_address = true
+
     tags {
-        Name = "${var.app_name}"
+        Name = "Nginx Example"
     }
 
     user_data = <<HEREDOC
--- single-machine-with-VPC/variables.tf
+++ single-machine-with-VPC/variables.tf
@@ -1,13 +1,9 @@
-variable "region" {}
+variable "region" {
+	default = "us-west-1"
+}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "aws_key_path" {}
-variable "aws_key_name" {}
+variable "ssh_key" {}
 variable "aws_ami" {
-	default = "ami-3a674d5a"
+	default = "ami-fce9d99c"
 }
-variable "docker_port" {}
-variable "docker_image" {}
-variable "app_name" {}
-variable "sumo_access_id" {}
-variable "sumo_access_key" {}
