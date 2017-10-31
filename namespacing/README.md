6067dccc3fd30d97b034e4e3eecc8994f69cadf3
Andrew Loesch <afloesch@gmail.com>
2017-10-31 14:33:06 -0700

    incremental checkin

--- namespacing/README.md
+++ namespacing/README.md
@@ -1,1 +1,15 @@
 # Namespacing different applications
+
+In the previous example we showed a good pattern for managing different environments with Terraform, and now let's build on that pattern with a way to isolate different applications from the core infrastructure.
+
+We will use the scripts from the modules example as a starting point. Let's use the network module to create our VPC, and then we will show how to namespace a single application server away from the core defined network assets. This way changes to the application Terraform scripts do not effect the core network, but still leverage those existing pieces.
+
+We have copied the example.tf, terraform.tfvars, and module folder from the modules example. Next we created a new directory for our application called "application," and a main.tf file inside that directory for the application specific Terraform scripts. By splitting the application from the rest of our Terraform assets we need to run Terraform multiple times to build everything. The application scripts are dependent on the core infrastructure being available, so create the VPC first. From this directory run:
+
+```
+terraform init
+terraform get
+terraform apply
+```
+
+With the core VPC created, let's take a look at the application/main.tf file and see how we can add an application to the already created VPC.
---
+++ namespacing/application/main.tf
@@ -0,0 +1,69 @@
+variable "region" {
+	default = "us-west-1"
+}
+variable "ssh_key" {}
+variable "aws_ami" {
+	default = "ami-02eada62"
+}
+
+# import the cert into amazon for accessing the boxes
+resource "aws_key_pair" "dev" {
+  key_name   = "aws.test"
+  public_key = "${file(var.ssh_key)}"
+}
+
+# create a security group to expose web traffic ports to public
+resource "aws_security_group" "web" {
+  name        = "Web Traffic"
+  description = "Allow all inbound traffic from http (80)"
+  vpc_id = "${aws_vpc.default.id}"
+
+  tags {
+    Name = "Web Traffic"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+
+# create base boxes to serve application
+resource "aws_instance" "node" {
+    ami = "${var.aws_ami}"
+    availability_zone = "us-west-1a"
+    instance_type = "t2.small"
+    key_name = "${aws_key_pair.dev.key_name}"
+    vpc_security_group_ids = ["${aws_security_group.web.id}"]
+    subnet_id = "${aws_subnet.us-west-1a-public.id}"
+
+    tags {
+        Name = "Nginx Example"
+    }
+
+    user_data = <<HEREDOC
+    #!/bin/bash
+    sudo su
+    yum update -y
+    yum install -y docker
+    service docker start
+    docker run -p 80:80 -d nginx
+    HEREDOC
+}
